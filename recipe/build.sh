3e250966839b2a1a7c9742f258f6a03208eb28a0
Valerio Mariani <valmar@slac.stanford.edu>
2022-11-25 16:44:21 -0800

    Migration to python 3.9, take 2

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,7 @@ else
 fi
 
 
-export LDFLAGS="-lrt"
+export LDFLAGS="-lrt -lstdc++"
 export PKG_CONFIG_PATH=${CONDA_PREFIX}/share/pkgconfig:${CONDA_PREFIX}/lib/pkgconfig:$PKG_CONFIG_PATH
 
 env |sort
@@ -20,9 +20,10 @@ mkdir build
 cd build
 ${CONDA_PREFIX}/bin/cmake --version
 ${CONDA_PREFIX}/bin/cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-  -DSZ3_USE_BUNDLED_ZSTD=OFF \
-  -DSZ3_DEBUG_TIMINGS=OFF "${CMAKE_PLATFORM_FLAGS[@]}" \
-  "${SRC_DIR}"
+  "${CMAKE_PLATFORM_FLAGS[@]}" \
+  -DFPZIP_FP=FPZIP_FP_SAFE \
+  -DBUILD_UTILITIES=OFF \
+  ${SRC_DIR}
 
 ${CONDA_PREFIX}/bin/make -j${CPU_COUNT}
 ${CONDA_PREFIX}/bin/make install PREFIX=${PREFIX}
